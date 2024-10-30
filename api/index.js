8a63e75a7b85f7ba13d0cfd9667e05cd5d20be1c
MATEUS SCHMIT DE LIMA <mateus.28964@aluno.pr.senac.br>
2024-10-30 17:13:54 -0300

    atualização4

--- api/index.js
+++ api/index.js
@@ -2,22 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const admin = require("firebase-admin");
 
-// Configuração do Firebase com variáveis de ambiente
 const serviceAccount = {
-    type: "service_account",
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Para corrigir quebra de linha
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${process.env.FIREBASE_CLIENT_EMAIL}`
+    // Suas credenciais aqui
 };
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Use a porta fornecida pelo Vercel
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
@@ -44,7 +34,7 @@ app.get('/cartoes', async (req, res) => {
 app.post('/cartoes', async (req, res) => {
     const { Nome, Valor, Imagem } = req.body;
     if (!Nome || !Valor || !Imagem) {
-        return res.status(400).json({ mensagem: 'Dados incompletos para cadastro do cartão' });
+        return res.status(400).json({ mensagem: 'Dados inválidos para cadastro do cartão' });
     }
     try {
         await db.collection('cartoes').add({
@@ -76,13 +66,13 @@ app.delete('/cartoes/:id', async (req, res) => {
 app.put('/cartoes/:id', async (req, res) => {
     const id = req.params.id;
     const { Nome, Valor, Imagem } = req.body;
-    const dadosAtualizados = { Nome, Valor, Imagem };
+    const dadosAtualizados = {};
+    if (Nome) dadosAtualizados.Nome = Nome;
+    if (Valor) dadosAtualizados.Valor = Valor;
+    if (Imagem) dadosAtualizados.Imagem = Imagem;
+
     try {
         const cartaoRef = db.collection('cartoes').doc(id);
-        const doc = await cartaoRef.get();
-        if (!doc.exists) {
-            return res.status(404).json({ mensagem: `Cartão com ID ${id} não encontrado` });
-        }
         await cartaoRef.update(dadosAtualizados);
         res.status(200).json({ mensagem: `Cartão com ID ${id} atualizado com sucesso` });
     } catch (e) {
@@ -91,4 +81,5 @@ app.put('/cartoes/:id', async (req, res) => {
     }
 });
 
+// Exportando o app para ser utilizado pelo Vercel
 module.exports = app;
